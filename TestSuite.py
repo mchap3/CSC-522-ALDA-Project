a340577b85234e73d11021de763aa0221b1066b0
ColonelPolonel <epurnell1996@gmail.com>
2022-10-10 00:47:50 -0400

    Some indicator calcs, extra imports added to test suite, some junk in test suite from me running tests

--- TestSuite.py
+++ TestSuite.py
@@ -2,7 +2,8 @@
 Place to build functions and/or test out the program pieces and try joining them together.
 '''
 import pandas as pd
-
+import indicators
+import visualization
 import datamanipulation
 import datetime as dt
 
@@ -10,5 +11,4 @@ dailydf = datamanipulation.retrieve()
 weeklydf = datamanipulation.retrieve(timeframe='weekly')
 monthlydf = datamanipulation.retrieve(timeframe='monthly')
 
-
-
+print(indicators.ema(dailydf))
--- indicators.py
+++ indicators.py
@@ -1,3 +1,38 @@
 '''
 Place to calculate any desirable indicators to incorporate into the machine learning bot.
 '''
+
+import pandas as pd
+import datamanipulation
+
+def sma(data, n=50, calc='close'):
+    '''
+    Calculates simple moving average.
+    :param data: dataframe with standard stock data (open, high, low, close, vol, date)
+    :param n: number of periods to be included in calculation
+    :param calc: data point to calculate sma (close is standard)
+    :return: original dataframe with additional column including SMA
+    '''
+    if 'time and date' in data.columns:
+        data = datamanipulation.timeformatter(data)
+    colname = 'SMA' + str(n)
+    data[colname] = data[calc].rolling(n).mean()
+
+    return data
+
+def ema(data, n=10, calc='close'):
+    '''
+    Calculates exponentiol moving average.
+    :param data: dataframe with standard stock data
+    :param n: number of periods
+    :param calc: data point to be used in the ema calculation
+    :return: dataframe with column appended for the ema
+    '''
+
+    if 'time and date' in data.columns:
+        data = datamanipulation.timeformatter(data)
+    colname = 'EMA' + str(n)
+    data[colname] = data[calc].ewm(span=n).mean()
+
+    return data
+
