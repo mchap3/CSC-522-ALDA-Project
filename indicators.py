86c142d064da779a743b078944fd70692a5ac47b
ColonelPolonel <epurnell1996@gmail.com>
2022-10-10 01:03:17 -0400

    Updated docstring with a little indicator commentary

--- indicators.py
+++ indicators.py
@@ -1,5 +1,22 @@
 '''
 Place to calculate any desirable indicators to incorporate into the machine learning bot.
+Thinking it might be good to add quite a few... preferably one or two from each class.
+From https://www.investopedia.com/articles/active-trading/011815/top-technical-indicators-rookie-traders.asp#:~:text=In%20general%2C%20technical%20indicators%20fit,strength%2C%20volume%2C%20and%20momentum.
+    5 categories of indicator:
+        trend indicators
+        mean reversion
+        relative strength
+        momentum
+        volume
+    and each category can be subdivided into leading and lagging:
+        leading attempt to predict where price is going
+        lagging offer a historical perspective
+    a few examples
+        1) moving averages: trend indicators, lagging
+        2) bollinger bands: mean reversion indicator, lagging
+        3) stochastic RSI: relative strength indicator, leading
+        4) MACD: momentum indicator, generally lagging, sometimes leading
+        5) on-balance volume: volume indicator, leading
 '''
 
 import pandas as pd
