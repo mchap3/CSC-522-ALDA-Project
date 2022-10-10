8eccc12818f72a7242add2610e0e77a276f4a025
ColonelPolonel <epurnell1996@gmail.com>
2022-10-10 00:16:51 -0400

    Added a few other points of interest (midpoint, center, and centroid) that we may want to feed to indicators for smoothing and such.

--- datamanipulation.py
+++ datamanipulation.py
@@ -49,3 +49,35 @@ def date_mask(df, start_date, end_date):
     data = df.loc[mask]
     # data['date'] = pd.to_datetime(data['date'])
     return data
+
+    
+def mid(data):
+    '''
+    :param data: stock data to work with
+    :return: dataframe with midpoint appended (mean of high and low)
+    '''
+
+    data['mid'] = (data['high'] + data['low'])/2
+
+    return data
+
+def center(data):
+    '''
+    :param data: stock data to work with
+    :return: dataframe with center appended (mean of open and close)
+    '''
+
+    data['center'] = (data['open'] + data ['close']) / 2
+
+    return data
+
+
+def centroid(data):
+    '''
+    :param data: stock data to work with
+    :return: dataframe with centroid appended (mean of open, high, low, and close)
+    '''
+
+    data['centroid'] = (data['open'] + data['high'] + data['low'] + data['close']) / 4
+
+    return data
