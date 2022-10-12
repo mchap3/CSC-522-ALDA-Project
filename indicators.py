6b2671dd528945bed9624c4c238192d71eb38906
bcmorri4 <bcmorri4@ncsu.edu>
2022-10-11 20:19:43 -0400

    Created a function five_Day_Centroid, in indicators.py that runs a 5-day moving average of the Centroid and then classifies as 1 (buy) or -1 (sell) if the 5-day average changed by more than ten cents and 0 if it didn't.

--- indicators.py
+++ indicators.py
@@ -1,3 +1,34 @@
 '''
 Place to calculate any desirable indicators to incorporate into the machine learning bot.
 '''
+
+import pandas as pd
+import datamanipulation
+import numpy as np
+from datamanipulation import *
+
+def centroid(data):
+    '''
+    :param data: stock data to work with
+    :return: dataframe with centroid appended (mean of open, high, low, and close)
+    '''
+
+    data['centroid'] = (data['open'] + data['high'] + data['low'] + data['close']) / 4
+
+    return data
+
+def five_Day_Centroid():
+    daily = retrieve()
+    daily = centroid(daily)
+    daily['Rolling5'] = daily['centroid'].rolling(5).mean()
+    daily['Rolling5_Buy'] = daily.Rolling5 > (daily.Rolling5.shift() + 0.1)
+    daily['Rolling5_Sell'] = daily.Rolling5 < (daily.Rolling5.shift() - 0.1)
+    daily['Buy_Sell'] = daily.Rolling5_Buy * 1 + daily.Rolling5_Sell * (-1)
+
+    print(daily[['centroid', 'Rolling5', 'Rolling5_Buy', 'Rolling5_Sell', 'Buy_Sell']].head(15))
+
+    return daily
+
+five_Day_Centroid()
+
+
