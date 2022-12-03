91d609a29d972eaa1b6d78ebf0df9177acceaf05
ColonelPolonel <epurnell1996@gmail.com>
2022-12-03 12:13:25 -0500

    Fixed issue with exceeding index limits in estimate_returns().

--- Validate_Analyze.py
+++ Validate_Analyze.py
@@ -5,7 +5,7 @@ A file for backtesting results, analyzing our data outputs, etc.
 
 import datamanipulation
 import indicators
-import MLcomponents
+# import MLcomponents
 import pandas as pd
 import numpy as np
 import math
@@ -15,17 +15,17 @@ import sys
 if not sys.warnoptions:
     import warnings
     warnings.simplefilter("ignore")
-
-dailydf = datamanipulation.retrieve()
-dailydf = datamanipulation.centroid(dailydf)
-dailydf = datamanipulation.center(dailydf)
-dailydf = datamanipulation.mid(dailydf)
-dailydf = datamanipulation.timeformatter(dailydf)
-
-start_date = '2002-01-01'
-end_date = '2012-01-01'
-mask = (dailydf['date'] >= start_date) & (dailydf['date'] <= end_date)
-data = dailydf.loc[mask]
+#
+# dailydf = datamanipulation.retrieve()
+# dailydf = datamanipulation.centroid(dailydf)
+# dailydf = datamanipulation.center(dailydf)
+# dailydf = datamanipulation.mid(dailydf)
+# dailydf = datamanipulation.timeformatter(dailydf)
+#
+# start_date = '2002-01-01'
+# end_date = '2012-01-01'
+# mask = (dailydf['date'] >= start_date) & (dailydf['date'] <= end_date)
+# data = dailydf.loc[mask]
 # print(data.to_string())
 # check = MLcomponents.cont_trend_label(data, w=0.02)
 # print(check.to_string())
@@ -93,7 +93,8 @@ def active_return(data):
     '''
     initial_capital = 10000
     # First thing is to split the classed dataframe into trades
-    data['trade'] = data['Class'].ne(data['Class'].shift()).cumsum()
+    data['trade'] = data['class'].ne(data['class'].shift()).cumsum()
+    maxindex = max(data.index.values.tolist())
     df = data.groupby('trade')
     trades = []
     for name, value in df:
@@ -102,27 +103,30 @@ def active_return(data):
     # Now basically run the same things as the base case in estimate returns here, keep everything in a list of lists
     tradesummaries = []
     i = 1
+
+    # Iterate over each trade and determine results for each of them
     for trade in trades:
-        # print('Trade: ' + str(i))
         entry = []
         indices = trade.index.values.tolist()
-        # print(indices)
-        # cl = trade.loc[[indices[0]], 'Class']
-        # if cl.values[0] == 1.0:
-        #     buy = True
-        #     print('Buy')
-        # else:
-        #     buy = False
-        #     print('Sell')
-        maxindex = max(data.index.values.tolist())
-        # if len(indices) == 1 & (indices[0] + 1) < maxindex:
+
+        # Handle special cases first, mostly arising from a single day being assigned a particular class
         if len(indices) == 1:
-            trade.loc[indices[0] + 1] = data.loc[indices[0] + 1]
-            indices = trade.index.values.tolist()
+            # Handle the first case of there being a single trade, but more data to follow
+            if indices[0] < maxindex:
+                trade.loc[indices[0] + 1] = data.loc[indices[0] + 1]
+                indices = trade.index.values.tolist()
+
+            # The handle the case where the index value would exceed the available data if looking for a place to exit
+            # the trade. This isn't a great way to handle, but it basically negates the trade.
+            if indices[0] == maxindex:
+                trade.loc[indices[0] + 1] = trade.loc[indices[0]]
+                trade.reset_index(inplace=True)
+                indices = trade.index.values.tolist()
+
         # print(indices)
 
         # Determine class to do calcs for long vs short
-        cl = trade.loc[[indices[0]], 'Class']
+        cl = trade.loc[[indices[0]], 'class']
         if cl.values[0] == 1.0:
             buy = True
             # print('Buy')
@@ -246,68 +250,73 @@ def estimate_returns(data):
 # print(cont.to_string())
 # print(mov)
 #
-start1 = '2002-01-01'
-end1 = '2012-01-01'
-start2 = '2007-01-01'
-end2 = '2017-01-01'
-start3 = '2011-01-01'
-end3 = '2021-01-01'
-mask1 = (dailydf['date'] >= start1) & (dailydf['date'] <= end1)
-mask2 = (dailydf['date'] >= start2) & (dailydf['date'] <= end2)
-mask3 = (dailydf['date'] >= start3) & (dailydf['date'] <= end3)
-data1 = dailydf.loc[mask1]
-data2 = dailydf.loc[mask2]
-data3 = dailydf.loc[mask3]
+# start1 = '2002-01-01'
+# end1 = '2012-01-01'
+# start2 = '2007-01-01'
+# end2 = '2017-01-01'
+# start3 = '2011-01-01'
+# end3 = '2021-01-01'
+# mask1 = (dailydf['date'] >= start1) & (dailydf['date'] <= end1)
+# mask2 = (dailydf['date'] >= start2) & (dailydf['date'] <= end2)
+# mask3 = (dailydf['date'] >= start3) & (dailydf['date'] <= end3)
+# data1 = dailydf.loc[mask1]
+# data2 = dailydf.loc[mask2]
+# data3 = dailydf.loc[mask3]
+# print(data1)
+# datadf = pd.DataFrame(pd.read_excel('sample prediction.xlsx'))
+# datadf = datamanipulation.mid(datadf)
+# summaries = estimate_returns(datadf)
+# print(summaries)
 
-results = []
-for num in range(5, 50, 1):
-    entry = []
-    w = num / 1000
-    entry.append(w)
-    data1 = MLcomponents.cont_trend_label(data1, w=w)
-    summaries = estimate_returns(data1)
-    entry.append(summaries.iloc[0, 2])
-    results.append(entry)
+# results = []
+# for num in range(5, 50, 1):
+#     entry = []
+#     w = num / 1000
+#     entry.append(w)
+#     data1 = MLcomponents.cont_trend_label(data1, w=w)
+#     summaries = estimate_returns(data1)
+#     entry.append(summaries.iloc[0, 2])
+#     results.append(entry)
+#
+# summary1 = pd.DataFrame(results, columns=['w parameter', 'real return 2002-2012'])
+#
+# results = []
+# for num in range(5, 50, 1):
+#     entry = []
+#     w = num / 1000
+#     entry.append(w)
+#     data2 = MLcomponents.cont_trend_label(data2, w=w)
+#     summaries = estimate_returns(data2)
+#     entry.append(summaries.iloc[0, 2])
+#     results.append(entry)
+#
+# summary2 = pd.DataFrame(results, columns=['w parameter', 'real return 2007-2017'])
+#
+# results = []
+# for num in range(5, 50, 1):
+#     entry = []
+#     w = num / 1000
+#     entry.append(w)
+#     data3 = MLcomponents.cont_trend_label(data3, w=w)
+#     summaries = estimate_returns(data3)
+#     entry.append(summaries.iloc[0, 2])
+#     results.append(entry)
+#
+# summary3 = pd.DataFrame(results, columns=['w parameter', 'real return 2011-2021'])
+#
+# summary1['real return 2007-2017'] = summary2['real return 2007-2017']
+# summary1['real return 2011-2021'] = summary3['real return 2011-2021']
+# summary1.set_index('w parameter', inplace=True)
 
-summary1 = pd.DataFrame(results, columns=['w parameter', 'real return 2002-2012'])
-
-results = []
-for num in range(5, 50, 1):
-    entry = []
-    w = num / 1000
-    entry.append(w)
-    data2 = MLcomponents.cont_trend_label(data2, w=w)
-    summaries = estimate_returns(data2)
-    entry.append(summaries.iloc[0, 2])
-    results.append(entry)
-
-summary2 = pd.DataFrame(results, columns=['w parameter', 'real return 2007-2017'])
-
-results = []
-for num in range(5, 50, 1):
-    entry = []
-    w = num / 1000
-    entry.append(w)
-    data3 = MLcomponents.cont_trend_label(data3, w=w)
-    summaries = estimate_returns(data3)
-    entry.append(summaries.iloc[0, 2])
-    results.append(entry)
-
-summary3 = pd.DataFrame(results, columns=['w parameter', 'real return 2011-2021'])
-
-summary1['real return 2007-2017'] = summary2['real return 2007-2017']
-summary1['real return 2011-2021'] = summary3['real return 2011-2021']
-summary1.set_index('w parameter', inplace=True)
-
-fig, ax1 = plt.subplots()
-ax1.plot(summary1['real return 2002-2012'])
-ax1.plot(summary1['real return 2007-2017'])
-ax1.plot(summary1['real return 2011-2021'])
-ax1.set_ylabel('Real Return ($)')
-ax1.set_xlabel('w')
-plt.title('Real Decade Stock Returns vs. w Parameter')
-plt.legend(['2002-2012', '2007-2017', '2011-2021'])
-plt.show()
+# fig, ax1 = plt.subplots()
+# ax1.plot(summary1['real return 2002-2012'])
+# ax1.plot(summary1['real return 2007-2017'])
+# ax1.plot(summary1['real return 2011-2021'])
+# ax1.set_ylabel('Real Return ($)')
+# ax1.set_xlabel('w')
+# plt.title('Real Decade Stock Returns vs. w Parameter')
+# plt.legend(['2002-2012', '2007-2017', '2011-2021'])
+# plt.show()
 
 # print(summary1.to_string())
 # summaries = estimate_returns(data, w=0.02)
